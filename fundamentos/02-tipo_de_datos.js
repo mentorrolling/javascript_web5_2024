15b0521d219de922fb1fa978f6d633fa798853f2
Pablo Marino <pmarino2013@gmail.com>
2024-12-26 20:22:08 -0300

    Tipos de datos

---
+++ fundamentos/02-tipo_de_datos.js
@@ -0,0 +1,53 @@
+//Tipos de datos
+//Number
+let numPositivo = 10;
+let numNegativos = -356;
+let numFloat = 3.14;
+
+//String
+let school = "RollingCode";
+let numString = "22";
+let stringVacio = "";
+
+//Boolean
+let comprar = true;
+let comerEnCasa = false;
+
+//Undefined
+let cosita;
+
+//Null
+let billeteraDePablo = null;
+
+//NaN (Not a Number)
+let resultado = "Hola" * 3;
+
+//typeof
+// console.log(typeof numPositivo);
+
+//Escribe en consola
+console.log("Hola mundo");
+// console.log(numFloat);
+// console.warn("Cuidado");
+// console.error("Usted está mal de la cabeza");
+
+document.write(numNegativos); //Escribe en el navegador
+
+//ARRAYS o arreglos
+let alumnos = ["Juan", "Pedro", "Pablo", "Maria"];
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let cualquierCosa = ["Juan", 1, true, null, undefined, [1, 2, 3]];
+
+//Object o Objeto
+let persona = {
+  nombre: "Juan",
+  edad: 30,
+  soltero: true,
+  hijos: null,
+  mascotas: ["perro", "gato"],
+};
+
+// alert("Hola mundo");
+let numero = prompt("Ingrese un número");
+
+console.log(numero);
