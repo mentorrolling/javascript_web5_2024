57ea938a6b8c6c69389a3d166775d3d14fb217e2
Pablo Marino <pmarino2013@gmail.com>
2025-01-02 18:45:59 -0300

    Agrego 03-operadores.js

---
+++ fundamentos/03-operadores.js
@@ -0,0 +1,43 @@
+let numero1 = 22;
+let numero2 = 2;
+let numString = "22";
+//Operadores
+//Aritméticos
+//Suma
+console.log(numero1 + numero2);
+//Resta
+console.log(numero1 - numero2);
+//Multiplicación
+console.log(numero1 * numero2);
+//División
+console.log(numero1 / numero2);
+//resto
+console.log(numero1 % numero2);
+
+//Unitarios
+//++
+//--
+// console.log(++numero1);
+// console.log(--numero2);
+
+//Relacionales
+console.log(numero1 == numero2); //valor
+console.log(numero1 === numString); //valor y tipo
+console.log(numero1 > numero2);
+console.log(numero1 < numero2);
+console.log(numero1 >= numero2);
+console.log(numero1 <= numero2);
+console.log(numero1 !== numString); //distinto de en tipo y valor
+
+//Negación
+let activo = true;
+console.log(!activo);
+
+//lógicos
+//&& => AND y
+//OR
+let estudio = true;
+let practico = false;
+
+console.log(estudio && practico);
+console.log(estudio || practico);
