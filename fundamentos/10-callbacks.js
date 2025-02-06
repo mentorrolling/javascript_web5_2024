4123406fd9b8829c1b6690e8b89eb40c95cbbed4
Pablo Marino <pmarino2013@gmail.com>
2025-02-06 20:57:11 -0300

    Ejercicios de callbacks

--- fundamentos/10-callbacks.js
+++ fundamentos/10-callbacks.js
@@ -54,3 +54,90 @@ alumnos.forEach((alumno, index) =>
 
 //map
 const alumnosMayus = alumnos.map((alumno) => alumno.toUpperCase());
+
+/*
+Tarea 1
+Crea una función que reciba como dato ingresado del usuario tareas a realizar y los vaya almacenando en un array hasta que se cancele la operación o no se ingrese ninguna tarea más. Una vez cancelada debe devolver por consola la lista de tareas.
+
+algoritmo
+datos de entrada: tareas a realizar
+procesos: preguntar si se ingresa una tarea y si es así guardar en un array hasta cancelar la operación.
+salida: array con las tareas
+
+Crea una función que sirva de buscador de tareas. Se debe ingresar un termino y devolver las tareas que contengan ese termino entre sus caracteres.
+
+algoritmo
+entrada: término de búsqueda
+proceso: ingreso término, busco elementos del array que coincidan en parte con el término,mostrar el resultado
+salida: array con el resultado de la búsqueda o no
+
+*/
+const tareas = [];
+
+const toDoList = () => {
+  let tarea = ""; //false
+
+  do {
+    tarea = prompt("Ingrese una tarea");
+    //validar que se haya escrito algo
+    if (tarea) {
+      tareas.push(tarea);
+    }
+  } while (tarea); //mientras tarea sea distinto de vacio
+
+  return tareas;
+};
+
+//Buscador de tareas
+const buscarTareas = (termino = prompt("Ingrese el término a buscar")) => {
+  const resultado = tareas.filter((tarea) =>
+    tarea.toLowerCase().includes(termino.toLowerCase())
+  );
+  if (resultado.length > 0) {
+    return resultado;
+  } else {
+    return "No hay elementos en la búsqueda";
+  }
+};
+
+let resultadoDeBusqueda = buscarTareas();
+
+/*
+Tarea 2
+Crea una función que reciba como dato una palabra y retorne la misma palabra pero al revés.
+
+algoritmo
+entrada: la palabra o frase
+proceso: ingreso un dato de tipo string, 
+hola mundo " " split("") ["h","o","l","a"," ","m","u","n","d","o"].reverse()
+join("") "odnum "
+salida: la palabra al revés
+*/
+
+const palabraAlReves = function () {
+  let palabra = prompt("Ingrese palabra o frase");
+  //   let palabraInvertida = palabra.split("").reverse().join("");
+  return palabra.split("").reverse().join("");
+};
+
+/*
+Tarea 3
+Escribe una función anónima que reciba una palabra o frase como parámetro y retorne cuantas vocales contiene.
+
+algoritmo
+entrada: la frase o palabra
+proceso: recibo la frase y recorro caracter por caracter, comparar el caracter con las vocales
+salida: cantidad de vocales que hay en la frase
+
+*/
+
+const contadorDeVocales = function (
+  frase = prompt("Ingrese la frase o palabra")
+) {
+  const vocales = ["a", "e", "i", "o", "u", "á", "é", "í", "ó", "ú"];
+  let arrayCaracteres = frase.toLowerCase().split("");
+  const resultado = arrayCaracteres.filter((caracter) =>
+    vocales.includes(caracter)
+  );
+  return resultado.length;
+};
